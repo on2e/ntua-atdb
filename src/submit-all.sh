fbb12e08bc1f70ea2af449dab68193fe2991b468
George Thomadakis <gthomados@gmail.com>
2023-03-17 23:00:09 +0200

    Improve submit-all.sh script

--- src/submit-all.sh
+++ src/submit-all.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-[ -d "$ATDB_PROJECT_HOME" ] && cd $ATDB_PROJECT_HOME
-mkdir -p out logs
+set -eu
 
-shopt -s nullglob
-for f in src/main/*.py; do
-    base=$(basename "$f" .py)
-    echo -n "Submitting file $base.py..."
-    spark-submit "$f" > out/"$base".out 2> logs/"$base".log
-    sleep 3
-    echo
-done
-shopt -u nullglob
+if [ -d "$ATDB_PROJECT_HOME" ]; then
+    cd $ATDB_PROJECT_HOME
+    mkdir -p out logs
+
+    shopt -s nullglob
+    for f in src/main/*.py; do
+        base=$(basename "$f" .py)
+        echo -n "Submitting file $base.py..."
+        spark-submit "$f" >out/"$base".out 2>logs/"$base".log
+        sleep 3
+        echo
+    done
+    shopt -u nullglob
+fi
